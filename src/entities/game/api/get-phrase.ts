3bcf55d060834b78adeaa8392ff62d6b9d9d16c2
EvaSissy <sissyeva99@gmail.com>
2025-06-11 21:58:11 +0300

    release 110625

--- src/entities/game/api/get-phrase.ts
+++
@@ -1,9 +0,0 @@
-import { phrase } from '../config.ts'
-import type { GameContext } from '../context/game-context.ts'
-
-export const getPhrase = (): Pick<GameContext, 'phrase' | 'target'> => {
-  return {
-    phrase,
-    target: 10,
-  }
-}
--- src/widgets/clue.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { getGameContext } from '../entities/game/context/game-context.ts'
-
-const { phrase } = getGameContext()
-</script>
-
-<template>
-<h2>{{phrase}}</h2>
-</template>
